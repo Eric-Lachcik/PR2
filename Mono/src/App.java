415e861ec34694014294319527ed4cec915037fe
Eric-Lachcik <lachcikeric@gmail.com>
2023-11-14 20:02:34 +0100

    Trabajo hecho

--- Mono/src/App.java
+++ Mono/src/App.java
@@ -14,50 +14,63 @@ public class App {
         "Me alegra que asistieras a tu reunión familiar diaria.",
         "Qué apropiado, tú peleas como una vaca.",
         "Te habrá enseñado todo lo que sabes." };
+    int turno = 0;
     String resp = "---";
-    int Jugador1 = 4;
-    int Jugador2 = 4;
+    int Jugador1 = 0;
+    int Jugador2 = 0;
     Scanner scan = new Scanner(System.in);
     Random ram = new Random();
     System.out.println("Presione ENTER para elegir tu turno ");
-    String turno = scan.nextLine();  
+    String turnos = scan.nextLine();
     int alea = ram.nextInt(1, 3);
     int IA = ram.nextInt(1, 5);
     System.out.println("Tu turno es " + alea);
-    if (alea == 1) {
-      System.out.println("Empiezas primero, estas son tus opciones, elige del 1 al 4 ");
-      for (int n = 0; n < insultos.length; n++) {
-        System.out.println(resp + insultos[n]);
-      }
-      if (scan.hasNextInt()) {
-        int numeroIngresado = scan.nextInt();
-        System.out.println("Insulto seleccionado: " + insultos[numeroIngresado]);
-        System.out.println("Constestacion seleccionada: " + contestaciones[IA] );
-        if (numeroIngresado == IA) {
-          System.out.println("Has Perdido contra la IA ");
-        } else {
-          System.out.println("Has Ganando contra la IA ");
+    int valor = alea % 2;
+    while (turno < 4) {
+      if (valor == 0) {
+        System.out.println("Empiezas Primero, estas son tus opciones, elige del 1 al 4 ");
+        for (int n = 0; n < insultos.length; n++) {
+          System.out.println(resp + insultos[n]);
         }
-      }
-    } else {
-      System.out.println(IA + insultos[IA]);
-      System.out.println("Empiezas segundo, estas son tus opciones , elige del 1 al 4 ");
-      for (int m = 0; m < contestaciones.length; m++) {
-        System.out.println(resp + contestaciones[m]);
+        if (scan.hasNextInt()) {
+          int numeroIngresado = scan.nextInt();
+          System.out.println("Insulto seleccionado: " + insultos[numeroIngresado]);
+          System.out.println("Constestacion seleccionada: " + contestaciones[IA]);
+          if (numeroIngresado == IA) {
+            System.out.println("Has Perdido contra la IA ");
+            Jugador2++;
+          } else {
+            System.out.println("Has Ganando contra la IA ");
+            Jugador1++;
+          }
 
-      }
-      if (scan.hasNextInt()) {
-        int numeroIngresado = scan.nextInt();
-        System.out.println("Insulto seleccionado: " + insultos[IA]);
-        System.out.println("Contestacion seleccionada: " + contestaciones[numeroIngresado]);
-       
-        if (numeroIngresado == IA) {
-          System.out.println("Has Ganado contra la IA");
-        } else {
-          System.out.println("Has Perdido contra la IA");
         }
+        valor++;
+      } else {
+        System.out.println(resp + insultos[IA]);
+        System.out.println("Empiezas Segundo, estas son tus opciones , elige del 1 al 4 ");
+        for (int m = 0; m < contestaciones.length; m++) {
+          System.out.println(resp + contestaciones[m]);
+
+        }
+        if (scan.hasNextInt()) {
+          int numeroIngresado = scan.nextInt();
+          System.out.println("Insulto seleccionado: " + insultos[IA]);
+          System.out.println("Contestacion seleccionada: " + contestaciones[numeroIngresado]);
+
+          if (numeroIngresado == IA) {
+            System.out.println("Has Ganado contra la IA");
+            Jugador1++;
+          } else {
+            System.out.println("Has Perdido contra la IA");
+            Jugador2++;
+          }
+        }
+        valor = 0;
       }
+
+      turno++;
     }
-
+    System.out.println("Has ganado un total de: "  + Jugador1 + " veces. Y has perdido un total de: " + Jugador2 + " veces.");
   }
 }
