724c2f9b020b04301d769293ee0a6d31e97b9346
Eric Lachcik <lachcikeric@gmail.com>
2024-05-16 18:44:50 +0200

    First Adds

--- ProyectoFinal/src/App.java
+++ ProyectoFinal/src/App.java
@@ -1,25 +1,44 @@
 
 import java.sql.*;
+import javax.swing.*; 
+import java.awt.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Top10();
+    }
 
-        String url = "jdbc:mysql://localhost:3306/basejava";
-        try{
-            
-            Connection con=DriverManager.getConnection(url, "root", "");
+    public static void Top10() {
+        JTable table = new JTable();
+        JFrame frame = new JFrame("App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(new GridLayout(3, 1));
+
+        String tableTop10[] = {"Posicion", "Nombre"}; 
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/basejava";
+            Connection con = DriverManager.getConnection(url, "root", "");
 
             Statement myst = con.createStatement();
-            ResultSet rs = myst.executeQuery("select Nombre from equipo");
+            ResultSet rs1 = myst.executeQuery("select Posicion, Nombre from pilotos join clasificacion on pilotos.Id = clasificacion.Piloto_Id order by Puntos desc");
+            ResultSet rs = myst.executeQuery("select * from clasificacion");
 
-
-            while(rs.next()){
-                System.out.println(rs.getString("Nombre"));
+            while (rs.next()) {
+                String Posicion = String.valueOf(rs.getInt("Posicion"));
+                String Puntos = String.valueOf(rs.getInt("Puntos"));
+                String Nombre = rs1.getString("Nombre");
+                String Equipo_Nombre = rs.getString("Equipo_Nombre");
             }
-        }catch(SQLException e){
+            
+        } catch (SQLException e) {
             System.out.println(e);
         }
-        
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
 
     }
 }
